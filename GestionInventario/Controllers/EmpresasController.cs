08a57ac77d07edd3eb6d36d687acd9b553995128
jorge <jorge@DESKTOP-BGG1KFJ>
2018-11-02 01:03:21 -0300

    Actualizacion Kardex

---
+++ GestionInventario/Controllers/EmpresasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GestionInventario.Models;
+
+namespace GestionInventario.Controllers
+{
+    public class EmpresasController : Controller
+    {
+        private DB_A41D6A_GestionInventarioEntities db = new DB_A41D6A_GestionInventarioEntities();
+
+        // GET: Empresas
+        public ActionResult Index()
+        {
+            var empresa = db.Empresa.Include(e => e.Inventario).Include(e => e.Usuario);
+            return View(empresa.ToList());
+        }
+
+        // GET: Empresas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresa empresa = db.Empresa.Find(id);
+            if (empresa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresa);
+        }
+
+        // GET: Empresas/Create
+        public ActionResult Create()
+        {
+            ViewBag.idInventario = new SelectList(db.Inventario, "idInventario", "idInventario");
+            ViewBag.idUsuario = new SelectList(db.Usuario, "idUsuario", "nombreUsuario");
+            return View();
+        }
+
+        // POST: Empresas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idEmpresa,nombreEmpresa,direcicon,telefonoEmpresa,nacionalidad,idUsuario,idInventario")] Empresa empresa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Empresa.Add(empresa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idInventario = new SelectList(db.Inventario, "idInventario", "idInventario", empresa.idInventario);
+            ViewBag.idUsuario = new SelectList(db.Usuario, "idUsuario", "nombreUsuario", empresa.idUsuario);
+            return View(empresa);
+        }
+
+        // GET: Empresas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresa empresa = db.Empresa.Find(id);
+            if (empresa == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idInventario = new SelectList(db.Inventario, "idInventario", "idInventario", empresa.idInventario);
+            ViewBag.idUsuario = new SelectList(db.Usuario, "idUsuario", "nombreUsuario", empresa.idUsuario);
+            return View(empresa);
+        }
+
+        // POST: Empresas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idEmpresa,nombreEmpresa,direcicon,telefonoEmpresa,nacionalidad,idUsuario,idInventario")] Empresa empresa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empresa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idInventario = new SelectList(db.Inventario, "idInventario", "idInventario", empresa.idInventario);
+            ViewBag.idUsuario = new SelectList(db.Usuario, "idUsuario", "nombreUsuario", empresa.idUsuario);
+            return View(empresa);
+        }
+
+        // GET: Empresas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresa empresa = db.Empresa.Find(id);
+            if (empresa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresa);
+        }
+
+        // POST: Empresas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empresa empresa = db.Empresa.Find(id);
+            db.Empresa.Remove(empresa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
