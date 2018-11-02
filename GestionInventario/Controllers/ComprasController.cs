686f51f862a909e487f571b0512d2db98f52f8b1
jorge <jorge@DESKTOP-BGG1KFJ>
2018-11-01 23:12:15 -0300

    Creacion de Controladores y vistas

---
+++ GestionInventario/Controllers/ComprasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GestionInventario.Models;
+
+namespace GestionInventario.Controllers
+{
+    public class ComprasController : Controller
+    {
+        private DB_A41D6A_GestionInventarioEntities db = new DB_A41D6A_GestionInventarioEntities();
+
+        // GET: Compras
+        public ActionResult Index()
+        {
+            var compras = db.Compras.Include(c => c.Lote);
+            return View(compras.ToList());
+        }
+
+        // GET: Compras/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Compras compras = db.Compras.Find(id);
+            if (compras == null)
+            {
+                return HttpNotFound();
+            }
+            return View(compras);
+        }
+
+        // GET: Compras/Create
+        public ActionResult Create()
+        {
+            ViewBag.idLote = new SelectList(db.Lote, "idLote", "idLote");
+            return View();
+        }
+
+        // POST: Compras/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idCompra,cantidad,precioCompra,idLote")] Compras compras)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Compras.Add(compras);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idLote = new SelectList(db.Lote, "idLote", "idLote", compras.idLote);
+            return View(compras);
+        }
+
+        // GET: Compras/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Compras compras = db.Compras.Find(id);
+            if (compras == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idLote = new SelectList(db.Lote, "idLote", "idLote", compras.idLote);
+            return View(compras);
+        }
+
+        // POST: Compras/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idCompra,cantidad,precioCompra,idLote")] Compras compras)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(compras).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idLote = new SelectList(db.Lote, "idLote", "idLote", compras.idLote);
+            return View(compras);
+        }
+
+        // GET: Compras/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Compras compras = db.Compras.Find(id);
+            if (compras == null)
+            {
+                return HttpNotFound();
+            }
+            return View(compras);
+        }
+
+        // POST: Compras/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Compras compras = db.Compras.Find(id);
+            db.Compras.Remove(compras);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
